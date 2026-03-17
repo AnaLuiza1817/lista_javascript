9f6c68beab68d2e194ad65fece4899661c0704f3
AnaLuiza1817-2213 <ana_lb_carvalho@estudante.sesisenai.org.br>
2026-03-17 08:42:10 -0300

    add- Atividade 01 feita em sala

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+/*
 console.log("Hello, World!");
+let name = "Ana";
+let idade = "18";
+let altura = "1.55";
+console.log("Meu nome é " + name + ", tenho " + idade + " anos e minha altura é " + altura + " metros.");
+*/
