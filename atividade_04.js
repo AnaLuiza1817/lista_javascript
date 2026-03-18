2ce1cbd42f3e29c16caaeaeef76ea18fb0b8220e
AnaLuiza1817-2215 <ana_lb_carvalho@estudante.sesisenai.org.br>
2026-03-18 10:06:34 -0300

    Add- Atividade 04 realizada

---
+++ atividade_04.js
@@ -0,0 +1,10 @@
+let numeros = [];
+
+for (let i = 1; i <= 4; i++) {
+    let valor = parseInt(prompt(`Digite o ${i}º valor inteiro:`));
+    numeros.push(valor);
+}
+numeros.sort((a, b) => a - b);
+
+console.log ("Valores em ordem decrescente: ");
+console.log (numeros);
