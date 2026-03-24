db41c4c809fe5d34d5a061b23e6e648813302f8e
AnaLuiza1817-2213 <ana_lb_carvalho@estudante.sesisenai.org.br>
2026-03-24 07:59:47 -0300

    Add- Atiividade 08 finalizado

---
+++ Atividade_08.js
@@ -0,0 +1,11 @@
+let a = 8;
+let b = 5;
+
+let diferenca = a - b;
+console.log("a) Diferença:", diferenca);
+
+let resultado = (2 * a) + (3 * b);
+console.log("b) Dobro da primeira + triplo da segunda:", resultado);
+
+let multiplicacao = a * b;
+console.log("c) Multiplicação:", multiplicacao);
